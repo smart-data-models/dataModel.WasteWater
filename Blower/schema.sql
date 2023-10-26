64247b06ee73aaa28c3739029f8adfc3f158e430
Alberto Abella <alberto.abella@fiware.org>
2023-10-26 15:03:52 +0200

    test of SQL export

--- Blower/schema.sql
+++ Blower/schema.sql
@@ -1,3 +1,3 @@
 /* (Beta) Export of data model Blower of the subject dataModel.WasteWater for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Blower_type AS ENUM ('Blower');
-CREATE TABLE Blower (address JSON, airflow NUMERIC, airflowEstimation NUMERIC, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, endsAt TEXT, energy NUMERIC, name TEXT, owner JSON, pressure NUMERIC, source TEXT, startsAt TEXT, type Blower_type);
+CREATE TABLE Blower (address JSON, airflow NUMERIC, airflowEstimation NUMERIC, alternateName TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateModified TIMESTAMP, description TEXT, endsAt TEXT, energy NUMERIC, id TEXT PRIMARY KEY, location JSON, name TEXT, owner JSON, pressure NUMERIC, seeAlso JSON, source TEXT, startsAt TEXT, type Blower_type);
