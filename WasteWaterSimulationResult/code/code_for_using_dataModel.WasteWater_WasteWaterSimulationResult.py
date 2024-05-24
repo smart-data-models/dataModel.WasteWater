c2fbb0535779665e3c617416fa3bcff215024fb5
Alberto Abella <alberto.abella@fiware.org>
2024-05-24 17:28:07 +0200

    created example of code

--- WasteWaterSimulationResult/code/code_for_using_dataModel.WasteWater_WasteWaterSimulationResult.py
+++ WasteWaterSimulationResult/code/code_for_using_dataModel.WasteWater_WasteWaterSimulationResult.py
@@ -24,19 +24,24 @@
 #         curl -X GET http://localhost:1026/ngsi-ld/v1/entities?local=true&limit=1000
 #         
 #         # now the python code you can use to insert some value in the context broker according to the data model
+#         # Version Warning! 
+#         # This code is designed to work with the version 0.8 of pysmartdatamodels or later
+#         # to work with earlier version you need to replace the import instruction for
+#         # from pysmartdatamodels import pysmartdatamodels as sdm
 #         
-from pysmartdatamodels import pysmartdatamodels as sdm
+#         
+import pysmartdatamodels as sdm
 import subprocess
 serverUrl = "http://localhost:1026" # supposed that your broker is installed in localhost. Edit to match your configuration
 dataModel = "WasteWaterSimulationResult"
 subject = "dataModel.WasteWater"
-nh4 = {'type': 'Property', 'value': 1.83, 'providedBy': {'type': 'Relationship', 'object': 'urn:ngsi-ld:WasteWaterTank:aerobicTank02'}}
+nh4 = 1.83
 attribute = "nh4"
 value = nh4
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
 print(sdm.update_broker(dataModel, subject, attribute, value, serverUrl=serverUrl, updateThenCreate=True))
 
-no3 = {'type': 'Property', 'value': 6.27, 'providedBy': {'type': 'Relationship', 'object': 'urn:ngsi-ld:WasteWaterTank:aerobicTank02'}}
+no3 = 6.27
 attribute = "no3"
 value = no3
 # The next line creates the query for inserting this attribute in a NGSI-LD context broker if the attribute does not exist it creates it
